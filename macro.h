520cc9cf5c7cd1a851c979529800ca07ae851a47
Danylo <Danylo.Brudnyi.RI.2023@lpnu.ua>
2023-11-26 18:55:56 +0200

    first commit

---
+++ macro.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#pragma once
+
+#define z 6
+#define SQR(x) (x)*(x)
+#define MAX(y,z) ((y)>(z))?(y):(z)
+#define MIN(y,z) ((y)<(z))?(y):(z)
+#define PRINTI(w) puts("..."); \
+ printf(#w"=%d\n",w)
